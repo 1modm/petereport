95ebf9ccfbd7787cb23e0f3bcc329a5a6647962c
Sebastien GAUTRIAS <sebastien.gautrias@thalesgroup.com>
2023-03-15 16:33:02 +0100

    Extract container start commands into external script

---
+++ docker/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd /opt/petereport && {
+    mkdir -p ./app/storage_reports/{html,images,jupyter,markdown,pdf} &&
+    pipenv install --deploy --ignore-pipfile --python 3.9 &&
+    pip freeze &&
+    pipenv run ./app/manage.py makemigrations &&
+    pipenv run ./app/manage.py migrate &&
+    pipenv run ./app/manage.py createfts &&
+    pipenv run ./app/manage.py loaddata ./app/config/cwe-list.json &&
+    pipenv run ./app/manage.py loaddata ./app/config/cwe-default.json &&
+    pipenv run ./app/manage.py loaddata ./app/config/owasp-list.json &&
+    pipenv run ./app/manage.py loaddata ./app/config/owasp-default.json &&
+    pipenv run gunicorn --chdir ./app petereport.wsgi:application --timeout 120 --graceful-timeout 60 --bind 127.0.0.1:8000
+}
