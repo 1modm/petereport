7d152c33d81b34b6c9e9e1cc662762455b373a06
Sebastien GAUTRIAS <sebastien.gautrias@thalesgroup.com>
2023-06-20 12:21:53 +0200

    repatch file upload

--- app/preport/utils/utils.py
+++ app/preport/utils/utils.py
@@ -1,6 +1,7 @@
 def build_report_file_name(template_name:str, type_name:str, report_name:str, report_date:str, report_extension:str) -> str:
-    report_n =  ''.join(e if e.isalnum() else '_' for e in report_name)
-    name_file = template_name + '_' + type_name + '_' + report_n + '_' + \
+    report_n =  ''.join(e if e.isalnum() or e.isspace() or e == '-' else '_' for e in report_name)
+    type_n =  ''.join(e if e.isalnum() or e.isspace()  or e == '-' else '_' for e in type_name)
+    name_file = template_name + '_' + type_n + '_' + report_n + '_' + \
             report_date + '.' + report_extension
 
     return name_file
